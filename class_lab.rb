daaa0e97516e1e7e456a68d044e59b16b157568f
Matt <matthew.r.manson@gmail.com>
2017-11-06 17:18:44 +0000

    completed parts A and B

--- class_lab.rb
+++ class_lab.rb
@@ -1,21 +1,36 @@
-# class Students
-#
-#   attr_accessor :name, :cohort, :statement, :favourite_language
-#   def initialize(name, cohort, statement, favourite_language)
-#     @name = name
-#     @cohort = cohort
-#     @statement = statement
-#     @favourite_language = favourite_language
-#   end
-#
-# end
+class Students
+
+  attr_accessor :name, :cohort, :statement, :favourite_language
+  def initialize(name, cohort, statement, favourite_language)
+    @name = name
+    @cohort = cohort
+    @statement = statement
+    @favourite_language = favourite_language
+  end
+
+end
 
 class Team
-  attr_accessor :name, :players, :coach
-  def initialize(name, players, coach)
+  attr_accessor :name, :players, :coach, :points
+  def initialize(name, players, coach, points)
     @name = name
     @players = players
     @coach = coach
+    @points = 0
+  end
+
+  def add_player(new_player)
+    @players << new_player
+    return @players
+  end
+
+  def check_players(name)
+    @players.include?(name)
+  end
+
+  def check_win_lose(win)
+    @points += 1 if win == true
+    return @points
   end
 
 end
--- specs/class_lab_spec.rb
+++ specs/class_lab_spec.rb
@@ -33,29 +33,67 @@ require_relative"../class_lab.rb"
 
 class TestTeam  <MiniTest::Test
 
-  def test_team_name
-    team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
-    assert_equal("footballz", team.name)
+  # def test_team_name
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   assert_equal("footballz", team.name)
+  # end
+  #
+  # def test_players
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   assert_equal(["jim", "bobby", "sam", "hitler"], team.players)
+  # end
+  #
+  # def test_coach
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   assert_equal("stalin", team.coach)
+  # end
+  #
+  # def test_rename_coach
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   team.coach = "mao"
+  #   assert_equal("mao", team.coach)
+  # end
+  #
+  # def test_new_player
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   result = team.add_player("new guy")
+  #   assert_equal(["jim", "bobby", "sam", "hitler", "new guy"], result)
+  # end
+  #
+  # def test_check_players_true
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   result = team.check_players("jim")
+  #   assert_equal(true, result)
+  # end
+  #
+  # def test_check_players_false
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   result = team.check_players("satan")
+  #   assert_equal(false, result)
+  # end
+
+  # def test_new_player
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   team.players << "new guy"
+  #   assert_equal(["jim", "bobby", "sam", "hitler", "new guy"], team.players)
+  # end
+
+  # def test_check_players
+  #   team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
+  #   result = team.players.include?("jim")
+  #   assert_equal(true, result)
+  # end
+
+  def test_points_win
+    team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin", 0)
+    result = team.check_win_lose(true)
+    assert_equal(1, result)
   end
 
-  def test_players
-    team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
-    assert_equal(["jim", "bobby", "sam", "hitler"], team.players)
+  def test_points_lose
+    team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin", 0)
+    result = team.check_win_lose(false)
+    assert_equal(0, result)
   end
-
-  def test_coach
-    team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
-    assert_equal("stalin", team.coach)
-  end
-
-  def test_rename_coach
-    team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
-    team.coach = "mao"
-    assert_equal("mao", team.coach)
-  end
-
-  def test_new_player
-    team = Team.new("footballz", ["jim", "bobby", "sam", "hitler"],"stalin")
-    
 
 end
